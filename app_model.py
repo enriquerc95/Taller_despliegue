2d7a682929b506b309b634f4319d30bd18f22e6f
enrikrecido <ramcoben95@gmail.com>
2024-04-24 12:43:57 +0200

    .
    .

--- app_model.py
+++ app_model.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
 
 # os.chdir(os.path.dirname(__file__))
-
+root_path = "/home/ramcoben/Taller_despliegue/"
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -20,7 +20,7 @@ def hello():
 @app.route('/api/v1/predict', methods=['GET'])
 def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
 
-    model = pickle.load(open('ad_model.pkl','rb'))
+    model = pickle.load(open(root_path + 'ad_model.pkl','rb'))
     tv = request.args.get('tv', None)
     radio = request.args.get('radio', None)
     newspaper = request.args.get('newspaper', None)
@@ -38,8 +38,8 @@ def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
 # Enruta la funcion al endpoint /api/v1/predict
 @app.route('/api/v1/retrain', methods=['GET'])
 def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
-    if os.path.exists("data/Advertising_new.csv"):
-        data = pd.read_csv('data/Advertising_new.csv')
+    if os.path.exists(root_path +"data/Advertising_new.csv"):
+        data = pd.read_csv(root_path +'data/Advertising_new.csv')
 
         X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
                                                         data['sales'],
@@ -51,7 +51,7 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
         rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
         mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
         model.fit(data.drop(columns=['sales']), data['sales'])
-        pickle.dump(model, open('ad_model.pkl', 'wb'))
+        pickle.dump(model, open(root_path + 'ad_model.pkl', 'wb'))
 
         return f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
     else:
